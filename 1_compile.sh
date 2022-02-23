fbc39e5a9857ecdeed48004020c03afa351cc04b
SimuIacrum <kazim-ali.mazhar@stud.hs-esslingen.de>
2022-02-23 20:02:31 +0000

    fit comment with rest of script

--- 1_compile.sh
+++ 1_compile.sh
@@ -1,5 +1,5 @@
-# for BOARD in iotlab-a8-m3 iotlab-m3 arduino-zero dwm1001 firefly frdm-kw41z microbit nrf51dk nrf52832-mdk nrf52840-mdk nrf52840dk nrf52dk nucleo-f070rb nucleo-wl55jc samd21-xpro samr21-xpro samr30-xpro samr34-xpro zigduino; do
-for BOARD in iotlab-m3; do  #for testing purposes
+#for testing purposes use iotlab-m3
+for BOARD in iotlab-a8-m3 iotlab-m3 arduino-zero dwm1001 firefly frdm-kw41z microbit nrf51dk nrf52832-mdk nrf52840-mdk nrf52840dk nrf52dk nucleo-f070rb nucleo-wl55jc samd21-xpro samr21-xpro samr30-xpro samr34-xpro zigduino; do
     for ROUNDS in 1000; do
         for MESSAGE_LENGTH in 128 256 512 1024; do
             for AUTH_DATA_LENGTH in 16 32 64 128; do
